424b1943ae47e8bd43ad1c80ac2b180b6499d0ee
sunil1890 <you@example.com>
2017-11-27 20:51:25 +0530

    swipe carousal installation and implementation

--- my-app/src/app/app-routing.module.ts
+++ my-app/src/app/app-routing.module.ts
@@ -2,7 +2,7 @@ import { NgModule }             from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 import { HomeComponent } from './home/home.component';
-import { AppRoutingModule } from './/app-routing.module';
+// import { AppRoutingModule } from './/app-routing.module';
 import { ServicesComponent } from './services/services.component';
 import { PricelistComponent } from './pricelist/pricelist.component';
 import { ContactusComponent } from './contactus/contactus.component';
@@ -12,7 +12,7 @@ const routes: Routes = [
   { path: 'home', component: HomeComponent },
   { path: 'services', component: ServicesComponent },
   { path: 'pricelist', component: PricelistComponent },
-  { path: 'contactus', component: ContactusComponent }
+  { path: 'contactus', component: ContactusComponent },
 ];
 @NgModule({
   exports: [ RouterModule ],
--- my-app/src/app/home/home.component.ts
+++ my-app/src/app/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import Swiper from 'swiper';
 
 @Component({
   selector: 'app-home',
@@ -10,6 +11,27 @@ export class HomeComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+  	 var swiper = new Swiper('.swiper-container', {
+      effect: 'coverflow',
+      loop:true,
+      speed: 1500,
+      grabCursor: true,
+      centeredSlides: true,
+      slidesPerView: 'auto',
+      coverflowEffect: {
+        rotate: 60,
+        stretch: 100,
+        depth: 100,
+        modifier: 1,
+        slideShadows : true,
+      },
+      pagination: {
+        el: '.swiper-pagination',
+      },
+      autoplay: {
+	    delay: 4000,
+	  },
+    });
   }
 
 }
